f3a002e9373d90e8a2b5b70b742eefb1dce19616
omsat <omsivasundar2010@gmail.com>
2025-06-01 21:11:16 -0400

    Completed Part 1

---
+++ Main.java
@@ -0,0 +1,58 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 8;
+        int intOperandB = 2;
+
+        int intS = intOperandA + intOperandB; // sum of integers
+
+        System.out.println(intOperandA + " plus " + intOperandB + " is " + intS);
+
+        int intP = intOperandA * intOperandB; // product of integers
+
+        System.out.println(intOperandA + " times " + intOperandB + " is " + intP);
+
+        int intD = intOperandA - intOperandB; // difference of integers
+
+        System.out.println(intOperandA + " minus " + intOperandB + " is " + intD);
+
+        int intQ = intOperandA / intOperandB; // quotient of integers
+
+        System.out.println(intOperandA + " divided by " + intOperandB + " is " + intQ);
+
+        int intM = intOperandA % intOperandB; // modulo of integers
+
+        System.out.println(intOperandA + " modulo " + intOperandB + " is " + intM);
+
+        System.out.println();
+
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 2.40;
+
+        double doubleS = doubleOperandA + doubleOperandB; // sum of the doubles
+
+        System.out.println(doubleOperandA + " plus " + doubleOperandB + " is " + doubleS);
+
+        double doubleP = doubleOperandA * doubleOperandB; // product of the doubles
+
+        System.out.println(doubleOperandA + " times " + doubleOperandB + " is " + doubleP);
+
+        double doubleD = doubleOperandA - doubleOperandB; // difference of the doubles
+
+        System.out.println(doubleOperandA + " minus " + doubleOperandB + " is " + doubleD);
+
+        double doubleQ = doubleOperandA / doubleOperandB; // quotient of the doubles
+
+        System.out.println(doubleOperandA + " divided by " + doubleOperandB + " is " + doubleQ);
+
+        double myLunchCost = 12.50;
+        int numKids = 2;
+        boolean isRaining = false;
+        double gasPrice = 2.94;
+        int favNumber = 3;
+        int shoeSize = 9;
+        String birthMonth = "October";
+        String fullName = "Om Sivasundar Sathishkumar";
+    }
+}
